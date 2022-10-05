c8591c01676145fb2fdb1677cdae1d81f729da16
jfmarting03 <jaimemartingil00@gmail.com>
2022-10-05 18:46:17 +0200

    david

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -24,9 +24,11 @@ import android.widget.TextView;
 public class MainActivity extends Activity {
 
 
-    private EditText operando1= (EditText) findViewById(R.id.ContenidoOperando1);
-    private EditText operando2 = (EditText) findViewById(R.id.ContenidoOperando2);
-    private TextView resultadoTexto;
+    public EditText operando1;
+    public EditText operando2;
+    public double operand1;
+    public double operand2;
+    public TextView resultadoTexto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);
@@ -34,10 +36,46 @@ public class MainActivity extends Activity {
         resultadoTexto=(TextView)findViewById(R.id.Resultado);
 
     }
-    private void suma(View vista){
-      int operand1=Integer.parseInt(operando1.getText().toString());
-      int operand2=Integer.parseInt(operando2.getText().toString());
-        resultadoTexto.setText(""+operand1+operand2);
+    public void suma(View vista){
+        EditText operando1= (EditText) findViewById(R.id.ContenidoOperando1);
+        EditText operando2 = (EditText) findViewById(R.id.ContenidoOperando2);
+       operand1=Integer.parseInt(operando1.getText().toString());
+      operand2=Integer.parseInt(operando2.getText().toString());
+        resultadoTexto.setText(""+(operand1+operand2));
+    }
+
+    public void resta(View vista){
+        EditText operando1= (EditText) findViewById(R.id.ContenidoOperando1);
+        EditText operando2 = (EditText) findViewById(R.id.ContenidoOperando2);
+         operand1=Double.parseDouble(operando1.getText().toString());
+         operand2=Double.parseDouble(operando2.getText().toString());
+        resultadoTexto.setText(""+(operand1-operand2));
+    }
+
+    public void multiplicacion(View vista){
+        EditText operando1= (EditText) findViewById(R.id.ContenidoOperando1);
+        EditText operando2 = (EditText) findViewById(R.id.ContenidoOperando2);
+         operand1=Double.parseDouble(operando1.getText().toString());
+         operand2=Double.parseDouble(operando2.getText().toString());
+        resultadoTexto.setText(""+(operand1*operand2));
+    }
+
+    public void division(View vista){
+        EditText operando1= (EditText) findViewById(R.id.ContenidoOperando1);
+        EditText operando2 = (EditText) findViewById(R.id.ContenidoOperando2);
+
+            operand1 = Double.parseDouble(operando1.getText().toString());
+            operand2 = Double.parseDouble(operando2.getText().toString());
+        if(operando1.getText().toString().isEmpty()) {
+            if (operand2 == 0) {
+                resultadoTexto.setText("NO SE PUEDE DIVIDIR ENTRE 0!!");
+
+            } else {
+                resultadoTexto.setText("" + (operand1 / operand2));
+            }
+        }else{
+            resultadoTexto.setText("Por favor, introduce un numero para poder operar!!");
+        }
     }
 
 
