9cf72ccd877a10a5378a09d7caca2dfbcfac75bd
jfmarting03 <jaimemartingil00@gmail.com>
2022-10-05 18:04:16 +0200

    david

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -18,17 +18,26 @@ import com.example.calculadora.databinding.ActivityMainBinding;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
-    private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
 
+    private EditText operando1= (EditText) findViewById(R.id.ContenidoOperando1);
+    private EditText operando2 = (EditText) findViewById(R.id.ContenidoOperando2);
+    private TextView resultadoTexto;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);
        setContentView(R.layout.activity_main);
-//jaime eres una puta
+        resultadoTexto=(TextView)findViewById(R.id.Resultado);
+
+    }
+    private void suma(View vista){
+      int operand1=Integer.parseInt(operando1.getText().toString());
+      int operand2=Integer.parseInt(operando2.getText().toString());
+        resultadoTexto.setText(""+operand1+operand2);
     }
 
 
