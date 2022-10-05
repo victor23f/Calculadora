0b98064f6a4ae0e63d386a0023411cf04166bb4f
david-mateosl <82931791+david-mateosl@users.noreply.github.com>
2022-10-05 17:37:49 +0200

    interfaz grafica

--- app/src/main/java/com/example/calculadora/MainActivity.java
+++ app/src/main/java/com/example/calculadora/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
        super.onCreate(savedInstanceState);
        setContentView(R.layout.activity_main);
-
+//jaime eres una puta
     }
 
 
